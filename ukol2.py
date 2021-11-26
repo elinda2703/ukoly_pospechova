e4a6470c7bc3e883d4d4fb53b7ea50aa462d1c05
elinda <ela.pospechova@gmail.com>
2021-11-26 15:48:33 +0100

    kinda to funguje, ale je to nechutně napsaný

--- ukol2.py
+++ ukol2.py
@@ -1,8 +1,8 @@
 import csv
-with open(r"ukoly\vstup.csv", encoding="utf-8") as csvinfile, open(r"ukoly\vystup_7denni.csv", "w", encoding="utf-8", newline="") as csvoutfile:
+with open(r"vstup.csv", encoding="utf-8") as csvinfile, open(r"vystup_7denni.csv", "w", encoding="utf-8", newline="") as csvoutfile, open(r"vystup_rok.csv", "w", encoding="utf-8", newline="") as csvoutfile_rok:
     reader = csv.reader(csvinfile, delimiter=",")
     writer = csv.writer(csvoutfile)
-    writer_rok = csv.writer(csvoutfile)
+    writer_rok = csv.writer(csvoutfile_rok)
     n=0
     m=0
     sedm_prutoku=[] #sem se nacte 7 prutoku z jednoho tydne
@@ -13,6 +13,7 @@ with open(r"ukoly\vstup.csv", encoding="utf-8") as csvinfile, open(r"ukoly\vystu
     seznam_rocnich_prumeru=[]
     ostatni_udaje=[]
     seznam_rocnich_prutoku=[]
+    ostatni_udaje_rok=[]
     
     for row in reader:
         n+=1
@@ -34,18 +35,27 @@ with open(r"ukoly\vstup.csv", encoding="utf-8") as csvinfile, open(r"ukoly\vystu
             seznam_rocnich_prutoku.append(float(row[5]))
 
         else:
+            ostatni_udaje_rok.append(prutoky_rok[0])
             prumer = round((sum(seznam_rocnich_prutoku)/len(seznam_rocnich_prutoku)),4)
             seznam_rocnich_prumeru.append(prumer)
+            seznam_rocnich_prutoku.clear()
             prutoky_rok.clear()
-        
-        
+            prutoky_rok.append(row[0:5])
+            seznam_rocnich_prutoku.append(float(row[5]))
+
+    prumer = round((sum(seznam_rocnich_prutoku)/len(seznam_rocnich_prutoku)),4)    
+    seznam_rocnich_prumeru.append(prumer)
+    ostatni_udaje_rok.append(prutoky_rok[0])    
    
    
-    print(seznam_rocnich_prutoku)
+    
     for i in range (len(seznam_prumeru)):
         ostatni_udaje[i].append(seznam_prumeru[i])
-
-    writer.writerows(ostatni_udaje)
     
+    for i in range (len(seznam_rocnich_prumeru)):
+        ostatni_udaje_rok[i].append(seznam_rocnich_prumeru[i])
+    print(ostatni_udaje_rok)
+    writer.writerows(ostatni_udaje)
+    writer_rok.writerows(ostatni_udaje_rok)
 
         
