8ee55904a525edb06fc927e1688c55ccb8bacc42
elinda <ela.pospechova@gmail.com>
2021-11-26 14:39:31 +0100

    strugglin

--- ukol2.py
+++ ukol2.py
@@ -1,24 +1,51 @@
 import csv
-import itertools
-with open("vstup.csv", encoding="utf-8") as csvinfile,\
-	open("vystup_7denni.csv", "w", encoding="utf-8") as csvoutfile:
+with open(r"ukoly\vstup.csv", encoding="utf-8") as csvinfile, open(r"ukoly\vystup_7denni.csv", "w", encoding="utf-8", newline="") as csvoutfile:
     reader = csv.reader(csvinfile, delimiter=",")
     writer = csv.writer(csvoutfile)
-    next(reader)
+    writer_rok = csv.writer(csvoutfile)
     n=0
     m=0
-    average=0
-    prutok = 0
+    sedm_prutoku=[] #sem se nacte 7 prutoku z jednoho tydne
+    ostatni_udaje=[]
+    seznam_prumeru=[]
+    seznam_za_rok=[]
+    prutoky_rok=[]
+    seznam_rocnich_prumeru=[]
+    ostatni_udaje=[]
+    seznam_rocnich_prutoku=[]
+    
     for row in reader:
         n+=1
-        prutok += float(row[5])
+        m+=1
+        rok=int(row[2])
+ 
+        sedm_prutoku.append(float(row[5])) 
+
+        if m%7==0:
+            prumer = round((sum(sedm_prutoku)/len(sedm_prutoku)),4)
+            seznam_prumeru.append(prumer)
+            sedm_prutoku.clear()
+        
         if n%7==1:
-            sum_prutoku = round(prutok, 4)
-            writer.writerow([row[0:5], sum_prutoku])
-            prutok = 0
-            sum_prutoku = 0
-        while True:
-            m+=1
-            average+=float(row[5])
-            if m%7==0:
-                break
+            ostatni_udaje.append(row[0:5])
+        
+        if len(prutoky_rok)==0 or rok==int(prutoky_rok[-1][2]):
+            prutoky_rok.append(row[0:5])
+            seznam_rocnich_prutoku.append(float(row[5]))
+
+        else:
+            prumer = round((sum(seznam_rocnich_prutoku)/len(seznam_rocnich_prutoku)),4)
+            seznam_rocnich_prumeru.append(prumer)
+            prutoky_rok.clear()
+        
+        
+   
+   
+    print(seznam_rocnich_prutoku)
+    for i in range (len(seznam_prumeru)):
+        ostatni_udaje[i].append(seznam_prumeru[i])
+
+    writer.writerows(ostatni_udaje)
+    
+
+        
